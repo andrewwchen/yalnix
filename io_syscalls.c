e02eb7576be7ea4cd5134e4af283cba792c5edb2
Andrew W. Chen <andrewac2015@gmail.com>
2024-01-29 12:52:36 -0500

    checkpoint 1

---
+++ io_syscalls.c
@@ -0,0 +1,31 @@
+// Contains io syscalls as defined in yalnix.h
+//
+// Tamier Baoyin, Andrew Chen
+// 1/2024
+
+int
+TtyRead(int tty_id, void *buf, int len)
+{
+  // Read the next line of input from terminal tty id,
+  // copying it into the buffer referenced by buf.
+  // The maximum length of the line to be returned is given by len.
+  // The line returned in the buffer is not null-terminated.
+  // If there are sufficient unread bytes already waiting,
+  // the call will return right away, with those.
+  // Otherwise, the calling process is blocked until a line of input is available to be returned.
+  // If the length of the next available input line is longer than len bytes,
+  // only the first len bytes of the line are copied to the calling process,
+  // and the remaining bytes of the line are saved by the kernel for the next TtyRead (by this or another process).
+  // If the length of the next available input line is shorter than len bytes,
+  // only as many bytes are copied to the calling process as are available in the input line;
+  // On success, the number of bytes actually copied into the calling process’s buffer is returned;
+  // in case of any error, the value ERROR is returne
+}
+int TtyWrite(int tty_id, void *buf, int len)
+{
+  // Write the contents of the buffer referenced by buf to the terminal tty id.
+  // The length of the buffer in bytes is given by len.
+  // The calling process is blocked until all characters from the buffer have been written on the terminal.
+  // On success, the number of bytes written (len) is returned; in case of any error, the value ERROR is returned.
+  // Calls to TtyWrite for more than TERMINAL MAX LINE bytes should be supported. 
+}
---
+++ traps.c
@@ -0,0 +1,67 @@
+// Contains trap handlers to be placed in the interrupt vector
+//
+// Tamier Baoyin, Andrew Chen
+// 1/2024
+
+#include <ykernel.h>
+
+void
+TrapKernel(UserContext uc)
+{
+  int syscall_number = uc.code;
+  // Arguments are in the user context registers, regs = uc.regs[gregs]
+  // Makes the corresponding syscall to the syscall_number (see syscalls section)
+}
+
+void
+TrapClock(UserContext uc)
+{
+  // Use round-robin scheduling to context switch to the next process in the ready queue if it exists
+  // Else run the idle process
+}
+
+void
+TrapIllegal(UserContext uc)
+{
+  // Type of illegal instruction = uc.code
+  // Traceprintf 0 with process id and the type of illegal instruction
+  // Abort the process
+  exit(uc);
+}
+
+void
+TrapMemory(UserContext uc)
+{
+  /* Type of disallowed memory access = 
+  Enlarge the stack to cover uc.addr if it is:
+  In region 1
+  And below the currently allocated memory for the stack
+  And above the current brk(heap?)
+  Otherwise abort the process */
+}
+
+void TrapMath(UserContext uc)
+{
+  /* Type of math error = uc.code
+  Traceprintf 0 with process id and the type of math error
+  Abort the process, exit(uc) */
+}
+
+void TrapDisk(UserContext uc)
+{
+  // This trap is specific to an optional feature and will not be implemented
+}
+
+void TrapTTYTransmit(UserContext uc)
+{
+  // This trap handler responds to TRAP_TTY_TRANSMIT
+  // terminal number = uc.code
+  // indicates that user output is ready
+}
+
+void TrapTTYReceive(UserContext uc)
+{
+  // This trap handler responds to TRAP_TTY_RECEIVE
+  // terminal number = uc.code
+  // indicates that user input is ready
+}
