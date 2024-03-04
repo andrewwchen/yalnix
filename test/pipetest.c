e10555d60e748999afe54850545dd0adfbbf47f4
Tianwen Chen <f004h5q@babylon1.thayer.dartmouth.edu>
2024-03-03 19:11:10 -0500

    pipetest final

--- test/pipetest.c
+++ test/pipetest.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
     // pipe buffer size: 256
     int pipe_id = -2;
     int *pipe_idp = &pipe_id;
-    if(LockInit(pipe_idp) == -1){
+    if(PipeInit(pipe_idp) == -1){
         TracePrintf(0, "===pipetest=== YOU FAILED\n");
     }
     const char *message = "AHHHHHHH";
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
     if(pid == 0){
         TracePrintf(0, "===pipetest=== Child\n");
         Delay(3);   // child delay writing for 3 seconds
-        int write_res = PipeWrite(pipe_id, (void*) message, sizeof(message));
+        int write_res = PipeWrite(pipe_id, (void*) message, 9*sizeof(char));
         if (write_res == -1){
             TracePrintf(0, "===pipetest=== CHILD: PipeWrite FAILED\n");
             return -1;
@@ -29,12 +29,12 @@ int main(int argc, char *argv[]) {
         TracePrintf(0, "===pipetest=== CHILD: wrote to pipe\n");
 
     } else {
-        TracePrintf(0, "===pipetest=== Parent");
-        char buffer_read[100];   
-        // char buffer_read[5]; 
+        TracePrintf(0, "===pipetest=== Parent\n");
+        //char* buffer_read = malloc(100*sizeof(char));   
+        char* buffer_read = malloc(4*sizeof(char)); 
         // read from pipe, will be blocked until child writes
         // int PipeRead(int pipe id, void *buf, int len)
-        int read_res = PipeRead(pipe_id, (void*) buffer_read, sizeof(buffer_read));
+        int read_res = PipeRead(pipe_id, (void*) buffer_read, 4*sizeof(char));
         if (read_res == -1){
             TracePrintf(0, "===pipetest=== PARENT: PipeRead FAILED\n");
             return 1;
